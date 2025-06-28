57edd01ed9a95215c1c06265be0a37a650c0c0e1
khushi verma <145983965+Khushi-vermaa@users.noreply.github.com>
2025-06-28 13:29:33 +0530

    Update vite.config.js
    this is my chnages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/React-shopingplatfromUI/",
+  base: "/khushi-vermaa.github.io/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
